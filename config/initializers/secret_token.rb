1834819e226df07dac13b5c891c29244b00388c3
styrka <evan.p.lind@gmail.com>
2013-05-01 14:35:26 -0700

    init

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Murista::Application.config.secret_token = 'c8b3b7667d216bc5abd6e120ceeee97b30f4adeedd6351885c65aef91c58562969109ce9d01a4aae5b0d925fd9d7eeaea41f551ead47612815a8248aa6522c71'
